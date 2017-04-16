9d06c8391e1826b104befd0af295e83b17dc71bc
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-04-16 17:12:48 -0300

    add up commander

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
 var fs = require("fs");
+var program = require('commander')
+  .version(require('./package.json').version)
+//.option('-p, --peppers', 'Add peppers')
+  .parse(process.argv);
 
-var hljs = require('highlight.js');
-var md = require('markdown-it')({
-  html: true,
-  linkify: true,
-  highlight: function(str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(lang, str).value;
-      } catch (__) {}
-    }
-    try {
-      return hljs.highlightAuto(str).value;
-    } catch (__) {}
-    return ''; // use external default escaping
-  }
-});
-md.use(require('markdown-it-mathjax')());
-md.use(require('markdown-it-sub'));
-md.use(require('markdown-it-html5-embed'));
+//if (program.peppers) console.log('  - peppers');
+//console.log('  - %s cheese', program.cheese);
+console.log(' args: %j', program.args);
 
-var htmlBase = `<!DOCTYPE html>
-  <html>
-  <head>
-  <title>MathJax TeX Test Page</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./node_modules/github-markdown-css/github-markdown.css">
-  <style>.markdown-body {box-sizing: border-box;min-width: 200px;margin: 0 auto;padding: 45px;}</style>
-  <script type="text/javascript" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=AM_HTMLorMML"></script>
-  </head>
-  <body class="markdown-body">`;
-var htmlBase1 = "</body></html>";
-
+/*
 fs.readFile('./test.md', function(err, data) {
   if (err) throw err;
   fs.writeFile("./test.html", htmlBase + md.render(data.toString()) + htmlBase1, 'utf8');
-});
-fs.readFile('./latex.md', function(err, data) {
-  if (err) throw err;
-  fs.writeFile("./latex.html", htmlBase +md.render(data.toString() + htmlBase1), 'utf8');
 });
 fs.readFile('./source.md', function(err, data) {
   if (err) throw err;
   fs.writeFile("./source.html", htmlBase +md.render(data.toString() + htmlBase1), 'utf8');
 });
+*/
 
 // Error
 process.on('uncaughtException', function(err) {
---
+++ markdown-parser.js
@@ -0,0 +1,19 @@
+var hljs = require('highlight.js');
+var md = require('markdown-it')({
+  html: true,
+  linkify: true,
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (__) {}
+    return ''; // use external default escaping
+  }
+});
+md.use(require('markdown-it-mathjax')());
+md.use(require('markdown-it-sub'));
+md.use(require('markdown-it-html5-embed'));
